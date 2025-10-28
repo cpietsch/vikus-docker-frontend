83eac914ce5114cc55f1c4c7f6ceec12de4f1b9e
Christopher Pietsch <cpietsch@gmail.com>
2025-10-28 18:52:39 +0000

    refactor: simplify API fetch URL construction

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,12 +1,7 @@
-export const protocoll = 'http';
-export const domain = location.hostname;
-export const port = 5000
-export const portWeb = 8000;
-
 export function api(method, resource, data, params) {
 	// console.log(`${base}${resource}`);
 	const serializedParams = params ? `?${new URLSearchParams(params).toString()}` : '';
-	return fetch(`${protocoll}://${domain}:${port}${resource}${serializedParams}`, {
+	return fetch(`/api${resource}${serializedParams}`, {
 		method,
 		headers: {
 			'content-type': 'application/json'
